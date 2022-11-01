7bca0635dfad99a4d36fcf250f8bb830017a84f4
Bochoxic <vboscha@gmail.com>
2022-11-01 16:42:19 +0100

    Added Problem 4

---
+++ Problem_4.m
@@ -0,0 +1,3 @@
+[J4_1, J5_1] = JacobianMatrix(thetas_2_rad(1,1),thetas_2_rad(1,2),thetas_2_rad(1,3),thetas_2_rad(1,4));
+[J4_2, J5_2] = JacobianMatrix(thetas_2_rad(10,1),thetas_2_rad(10,2),thetas_2_rad(10,3),thetas_2_rad(10,4));
+[J4_3, J5_3] = JacobianMatrix(thetas_2_rad(28,1),thetas_2_rad(28,2),thetas_2_rad(28,3),thetas_2_rad(28,4));
