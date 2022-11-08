bd8b787981aa69ecddef13e1055a778d14b02c7d
Victor Vigara <vigaravictor99@gmail.com>
2022-11-08 14:51:37 +0100

    P6 started

--- Problem_5.m
+++ Problem_5.m
@@ -4,8 +4,8 @@ clc; clear all;
 % Hint: the last quantity x_ 4, you have not seen it as such in the course before, so you need to
 % think about how to interpret it in terms of angular velocities.
 
-phi = 2;
-v04 = [0; 0; 3];
+phi = pi/2;
+v04 = [0; -3; 0];
 w4 = [0; 0; 0];
 
 % Obtain coordinates for phi = 2
@@ -15,15 +15,22 @@ w4 = [0; 0; 0];
 [thetas_1_deg, thetas_2_deg, thetas_1_rad, thetas_2_rad] = jointAnglesCalculation(x,y,z);
 
 % Compute jacobian for phi = 2
-[J4, J5] = JacobianMatrix(thetas_1_rad(:,1), thetas_1_rad(:,2), thetas_1_rad(:,3), thetas_1_rad(:,4));
+[J4_1, J5_1] = JacobianMatrix(thetas_1_rad(:,1), thetas_1_rad(:,2), thetas_1_rad(:,3), thetas_1_rad(:,4));
+[J4_2, J5_2] = JacobianMatrix(thetas_2_rad(:,1), thetas_2_rad(:,2), thetas_2_rad(:,3), thetas_2_rad(:,4));
 
 % Inverse velocity calculation: As Jacobian matrix is not squared, the
 % inverse of the jacobian will be computed as: (J'*J)^-1 * J'
-J4_inv = (J4'*J4)^-1 * J4';
+J4_1_inv = (J4_1'*J4_1)^-1 * J4_1';
+J4_2_inv = (J4_2'*J4_2)^-1 * J4_2';
 
 % Joint velocities are calculated as q_dot = J_inv * [v_dot; w_dot]
-q_dot = J4_inv * [v04; w4];
+q_1_dot = J4_1_inv * [v04; w4];
+q_2_dot = J4_2_inv * [v04; w4];
 
-fprintf("Joint velocities for phi = 2: \n")
+fprintf("Joint velocities for phi = pi/2 - Conf1: \n")
 fprintf(" q1_dot = %f\n q2_dot = %f\n q3_dot = %f\n q4_dot = %f\n ", ...
-    q_dot(1), q_dot(2), q_dot(3), q_dot(4));
+    q_1_dot(1), q_1_dot(2), q_1_dot(3), q_1_dot(4));
+
+fprintf("Joint velocities for phi = pi/2 - Conf2: \n")
+fprintf(" q1_dot = %f\n q2_dot = %f\n q3_dot = %f\n q4_dot = %f\n ", ...
+    q_2_dot(1), q_2_dot(2), q_2_dot(3), q_2_dot(4));
